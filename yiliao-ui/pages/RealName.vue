13650b81c5436cca9a689d0ce9aab9aea62859d2
water <watershui122@gmail.com>
2023-05-20 15:29:24 +0700

    实名

--- yiliao-ui/pages/RealName.vue
+++ yiliao-ui/pages/RealName.vue
@@ -13,13 +13,15 @@
     <view class="wrap">
       <view class="from-input">
         <label>姓名</label>
-        <input type="text" class="input-text" placeholder="请输入真实姓名" />
+        <input type="text" v-model="realName" class="input-text" placeholder="请输入真实姓名" />
       </view>
       <view class="from-input">
         <label>身份证号码</label>
-        <input type="text" class="input-text" placeholder="请输入身份证号码" />
+        <input type="text" v-model="idCardNo" class="input-text" placeholder="请输入身份证号码" />
       </view>
-      <view class="btn-class">实名认证</view>
+      <van-button v-if="bindStatus" class="btn-class" block @click="changeBind" :loading="loading">
+        实名认证
+      </van-button>
     </view>
   </view>
 </template>
@@ -27,12 +29,31 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      loading: false,
+      idCardNo:'',//身份证号
+      realName:'',//真实姓名
+      bindStatus:'',//是否实名
+    };
+  },
+  onShow(){
+    this.getInfo()
   },
   methods: {
     // 返回上一页
     backPage() {
       window.history.back();
+    },
+    //用户列表数据
+    getInfo() {
+      this.$api.user_info().then((res) => {
+        if (res.data.code == 0) {
+          this.bindStatus = res.data.data.idCard;
+          console.log(this.bindStatus)
+          this.idCardNo = res.data.data.idCard;
+          this.realName = res.data.data.realName;
+        }
+      });
     },
   },
 };
