d1488110317db170380a777aba55479b72841cdb
water <watershui122@gmail.com>
2023-05-20 15:43:14 +0700

    实名

--- yiliao-ui/pages/RealName.vue
+++ yiliao-ui/pages/RealName.vue
@@ -13,11 +13,11 @@
     <view class="wrap">
       <view class="from-input">
         <label>姓名</label>
-        <input type="text" v-model="realName" class="input-text" placeholder="请输入真实姓名" />
+        <input type="text" :disabled="!bindStatus" v-model="realName" class="input-text" placeholder="请输入真实姓名" />
       </view>
       <view class="from-input">
         <label>身份证号码</label>
-        <input type="text" v-model="idCardNo" class="input-text" placeholder="请输入身份证号码" />
+        <input type="text" :disabled="!bindStatus" v-model="idCardNo" class="input-text" placeholder="请输入身份证号码" />
       </view>
       <van-button v-if="bindStatus" class="btn-class" block @click="changeBind" :loading="loading">
         实名认证
@@ -33,7 +33,7 @@ export default {
       loading: false,
       idCardNo:'',//身份证号
       realName:'',//真实姓名
-      bindStatus:'',//是否实名
+      bindStatus: false,//是否实名
     };
   },
   onShow(){
@@ -48,10 +48,33 @@ export default {
     getInfo() {
       this.$api.user_info().then((res) => {
         if (res.data.code == 0) {
-          this.bindStatus = res.data.data.idCard;
-          console.log(this.bindStatus)
+          if(res.data.data.idCard === null){
+            this.bindStatus = true
+          }else{
+            this.bindStatus = false
+          }
           this.idCardNo = res.data.data.idCard;
           this.realName = res.data.data.realName;
+        }
+      });
+    },
+    // 实名认证
+    changeBind(){
+      if (!this.realName) {
+        return this.$base.show("请输入真实姓名~");
+      } else if(!this.idCardNo){
+        return this.$base.show("请输入身份证号码~");
+      }
+      this.loading = true
+      this.$api.user_realName(
+        {
+          realName: this.realName,
+          idCardNo: this.idCardNo,
+        }
+      ).then((res) => {
+        this.loading = false
+        if (res.data.code == 0) {
+          this.getInfo()
         }
       });
     },
