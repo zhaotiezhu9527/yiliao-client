d533aefcb2f709d167dad106442e61b4ea71283d
water <watershui122@gmail.com>
2023-05-24 15:39:40 +0700

    接口

--- yiliao-ui/plugins/base.js
+++ yiliao-ui/plugins/base.js
@@ -47,10 +47,10 @@ export const loading = (title) => {
 };
 const PATH_URL =
   process.env.NODE_ENV === "development"
-  ? "http://192.168.4.85:9522/"//本地测试
-  // ? "http://clientapi.juhai.xyz/"//测试
-  // : "/api/";//h5打包地址
-  : "http://clientapi.juhai.xyz/";//app打包地址
+  // ? "http://192.168.4.85:9522/"//本地测试
+  ? "http://www.juhai.top/api/"//测试
+  : "/api/";//h5打包地址
+  // : "http://www.juhai.top/api/";//app打包地址
 
 export const upload = (params) => {
   let header = {
