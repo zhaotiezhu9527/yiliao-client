a7ff2bd4e9d3459feb0f5fe60cca3acb4bda2b86
water <watershui122@gmail.com>
2023-06-20 18:17:05 +0700

    接口修改

--- yiliao-ui/plugins/base.js
+++ yiliao-ui/plugins/base.js
@@ -28,8 +28,8 @@ const PATH_URL =
     ? "http://192.168.4.85:9522/"//本地测试
     // ? "http://ym.qtapi.juhai.top/" //测试
     // : "/api/";//h5打包地址
-    // : "https://www.juhai.top/"; //杨杨a1 安科app打包地址
-    : "/DISOZzbHUGxkbPh2/"; //杨杨a1 安科h5打包地址
+    : "https://www.anke9988.com/DISOZzbHUGxkbPh2/"; //杨杨a1 安科app打包地址
+    // : "/DISOZzbHUGxkbPh2/"; //杨杨a1 安科h5打包地址
     // : "https://juhai.top/"; //阿明a2 安科app打包地址
     // : "https://juhai.top/"; //app打包地址测试
     // : "/ym-qtapi-test/"; //app打包地址测试
