66c197dd668e7658d7149b61b62602bdaf5a0d40
water <watershui122@gmail.com>
2023-08-04 13:59:41 +0700

    杨杨2 接口地址更换

--- yiliao-ui/plugins/base.js
+++ yiliao-ui/plugins/base.js
@@ -30,10 +30,11 @@ const PATH_URL =
       // : "/api/";//h5打包地址
       // : "https://anke9988.top/DISOZzbHUGxkbPh2/"; //杨杨a1 安科app打包地址
       // : "/DISOZzbHUGxkbPh2/"; //杨杨a1 安科h5打包地址
-	  // : "https://anke6666.top/QKDcq6giyFT232Eo/"; //杨杨a2 安科app打包地址
+	  : "https://gdsfg986.top/QKDcq6giyFT232Eo/"; //杨杨a2 安科app打包地址
       // : "/QKDcq6giyFT232Eo/"; //杨杨a2 安科h5打包地址
       // : "/lCY45gucOU2CMttF/"; //阿明a1 安科app打包地址
-      : "/gsLz2d6XZF2ptdoj/"; //阿明a2 安科app打包地址
+      // : "/gsLz2d6XZF2ptdoj/"; //阿明a2 安科h5打包地址
+	  // : "https://fads3498.top/gsLz2d6XZF2ptdoj/"; //阿明2 安科app打包地址
       // : "https://juhai.top/"; //app打包地址测试
       // : "/ym-qtapi-test/"; //app打包地址测试
     //  : "https://www.anke8811.com/lCY45gucOU2CMttF/"; //阿明a2 安科h5打包地址
