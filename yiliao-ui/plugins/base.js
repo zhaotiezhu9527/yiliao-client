ff2711da0c1388d6d8eb2aec8db35c4f535d1673
water <watershui122@gmail.com>
2023-08-05 12:13:30 +0700

    换域名

--- yiliao-ui/plugins/base.js
+++ yiliao-ui/plugins/base.js
@@ -30,7 +30,7 @@ const PATH_URL =
       // : "/api/";//h5打包地址
       // : "https://anke9988.top/DISOZzbHUGxkbPh2/"; //杨杨a1 安科app打包地址
       // : "/DISOZzbHUGxkbPh2/"; //杨杨a1 安科h5打包地址
-	  : "https://gdsfg986.top/QKDcq6giyFT232Eo/"; //杨杨a2 安科app打包地址
+	  : "https://www.tryegd.com/QKDcq6giyFT232Eo/"; //杨杨a2 安科app打包地址
       // : "/QKDcq6giyFT232Eo/"; //杨杨a2 安科h5打包地址
       // : "/lCY45gucOU2CMttF/"; //阿明a1 安科app打包地址
       // : "/gsLz2d6XZF2ptdoj/"; //阿明a2 安科h5打包地址
