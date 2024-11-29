0108a0f3695314483c26ac218c568d1f4c434cb7
zhaot0392 <zhaot0392@gmail.com>
2024-11-29 22:49:26 +0800

    111

--- api/src/main/java/com/juhai/api/utils/IpUtil.java
+++ api/src/main/java/com/juhai/api/utils/IpUtil.java
@@ -13,25 +13,28 @@ public class IpUtil {
     public static Map<String, String> IP_DETAIL_CACHE = new HashMap<>();
 
     static {
-//        BUFF = HttpUtil.downloadBytes("https://juhaijituan.oss-ap-southeast-1.aliyuncs.com/gs/ip2region.xdb");
-//        IP_DETAIL_CACHE.put("0:0:0:0:0:0:0:1", "本地");
+        try {
+            BUFF = HttpUtil.downloadBytes("https://juhaijituan.oss-ap-southeast-1.aliyuncs.com/gs/ip2region.xdb");
+            IP_DETAIL_CACHE.put("0:0:0:0:0:0:0:1", "本地");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String getIpDetail(String ip) {
-//        try {
-//            boolean hasKey = IP_DETAIL_CACHE.containsKey(ip);
-//            if (hasKey) {
-//                return IP_DETAIL_CACHE.get(ip);
-//            }
-//
-//            Searcher searcher = Searcher.newWithBuffer(BUFF);
-//            String ipDetail = searcher.search(ip);
-//            IP_DETAIL_CACHE.put(ip, ipDetail);
-//            return ipDetail;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        System.out.printf("测试");
+        try {
+            boolean hasKey = IP_DETAIL_CACHE.containsKey(ip);
+            if (hasKey) {
+                return IP_DETAIL_CACHE.get(ip);
+            }
+
+            Searcher searcher = Searcher.newWithBuffer(BUFF);
+            String ipDetail = searcher.search(ip);
+            IP_DETAIL_CACHE.put(ip, ipDetail);
+            return ipDetail;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         IP_DETAIL_CACHE.put(ip, "未知");
         return "未知";
     }
